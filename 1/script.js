a20e22f63b5bc79dcf1a15e11da895bc17d9b997
abmah <ab.mahasnh@gmail.com>
2022-12-23 21:26:51 +0200

    first commit

---
+++ 1/script.js
@@ -0,0 +1,44 @@
+//Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Adding the mesh to the scene
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 2;
+
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
